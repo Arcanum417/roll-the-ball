5b8aa2d5e630f201068d5a69c735e6e60b736cb5
Arcanum417 <lubos.len@gmail.com>
2017-12-04 21:37:19 +0100

    nope

--- src/App.js
+++ src/App.js
@@ -6,63 +6,11 @@ import './example-styles.css';
 //import './styles-resizable.css';
 const ReactGridLayout =  require('react-grid-layout');
 
-var xmlDoc;
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        myFunction(this);
-    }
-};
-xhttp.open("GET", "rollTheBall.xml", true);
-xhttp.send();
-
-function myFunction(xml) {
-    xmlDoc = xml.responseXML;
-    console.log(xmlDoc);
-    console.log(xmlDoc.getElementsByTagName("name")[0]);
-}
-
-const layoutStatic =
-    [
-        {i: '0', x: 0, y: 0, w: 1, h: 1},
-        {i: '1', x: 1, y: 0, w: 1, h: 1,static: true},
-        {i: '2', x: 2, y: 0, w: 1, h: 1},
-        {i: '3', x: 3, y: 0, w: 1, h: 1,},
-        {i: '4', x: 0, y: 1, w: 1, h: 1},
-        {i: 'f', x: 1, y: 1, w: 1, h: 1},
-        {i: 'g', x: 2, y: 1, w: 1, h: 1,},
-        {i: 'h', x: 3, y: 1, w: 1, h: 1},
-        {i: 'j', x: 1, y: 2, w: 1, h: 1},
-        {i: 'k', x: 2, y: 2, w: 1, h: 1},
-        {i: 'l', x: 3, y: 2, w: 1, h: 1},
-        {i: 'm', x: 0, y: 3, w: 1, h: 1},
-        {i: 'n', x: 1, y: 3, w: 1, h: 1},
-        {i: 'p', x: 3, y: 3, w: 1, h: 1, static: true, type: "D180"}
-    ];
-
-const layoutStatic2 =
-    [
-        {i: '0', x: 0, y: 3, w: 1, h: 1},
-        {i: '1', x: 1, y: 0, w: 1, h: 1,static: true},
-        {i: '2', x: 2, y: 0, w: 1, h: 1},
-        {i: '3', x: 3, y: 0, w: 1, h: 1,},
-        {i: '4', x: 0, y: 1, w: 1, h: 1},
-        {i: 'f', x: 1, y: 1, w: 1, h: 1},
-        {i: 'g', x: 2, y: 1, w: 1, h: 1,},
-        {i: 'h', x: 3, y: 1, w: 1, h: 1},
-        {i: 'j', x: 1, y: 2, w: 1, h: 1},
-        {i: 'k', x: 2, y: 2, w: 1, h: 1},
-        {i: 'l', x: 3, y: 2, w: 1, h: 1},
-        {i: 'm', x: 0, y: 3, w: 1, h: 1},
-        {i: 'n', x: 1, y: 3, w: 1, h: 1},
-        {i: 'p', x: 3, y: 3, w: 1, h: 1, static: true, type: "D180"}
-    ];
-
 var sizeW=4;
 var sizeH=4;
-const originalLayout = getFromLS('layout') || layoutStatic;
+const originalLayout = getFromLS('layout')
 class NoCompactingLayout extends React.PureComponent {
-
+/*
     resolveAfter2Seconds(x) {
         return new Promise(resolve => {
             setTimeout(() => {
@@ -73,76 +21,72 @@ class NoCompactingLayout extends React.PureComponent {
 
     async componentDidMount() {
         var d = await this.resolveAfter2Seconds(10)
-        console.log(xmlDoc);
         this.pickGame(0);
     }
-/*
-    componentDidMount(){
-        console.log(this.state.xmlDoc);
-        //this.pickGame(0);
-
-    }
 */
+    componentDidMount() {
+        var xhttp = new XMLHttpRequest();
+        var self = this
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                self.setState({ xmlDoc: this.responseXML })
+            }
+        };
+        xhttp.open("GET", "rollTheBall.xml", true);
+        xhttp.send();
+        console.log(this.state.xmlDoc);
+    }
+
     constructor(props) {
         super(props);
 
-
-        const layoutStatic3 =
-            [
-                {i: '0', x: 0, y: 0, w: 1, h: 1},
-            ];
         var score = 9;
 
         this.state = {
             //layout: JSON.parse(JSON.stringify(originalLayout)),
-            items: layoutStatic3,
+            items: [],
             layout: [],
             score,
-            layoutStatic3
         };
         this.onLayoutChange = this.onLayoutChange.bind(this);
-        this.resetLayout = this.resetLayout.bind(this);
-    }
-
-    resetLayout() {
-        this.setState({
-            layout: this.state.items
-        });
-        console.log("nope");
     }
 
     onLayoutChange(layout) {
         console.log("onLayoutChange!", layout);
         //saveToLS('layout', layout);
-        //this.setState({layout});
+        this.setState({layout});
         //this.props.onLayoutChange(layout);
 
     }
 
-   onDragStart(thing, thing2, thing3, thing4, thing5, thing6)   {
-        console.log(thing, thing2, thing3, thing4, thing5, thing6);
-    }
-/*
-    generateDOM() {
-        return _.map(_.range(this.props.items), function(i) {
-            return (<div key={i}><span className="text">{i}</span></div>);
-        });
-    }
-*/
-    /*
-            generateDOM() {
-                console.log("generateDOM");
-                var textArray = [
-                    'direct.png',
-                    'turn.png',
-                    'box.png'
-                ];
-                var randomNumber = Math.floor(Math.random()*textArray.length);
+    checkWin(gameSelectionIndex)
+    {
+        var game = this.state.xmlDoc.getElementsByTagName('rolltheball')[0].getElementsByTagName('games')[0].getElementsByTagName('game')[gameSelectionIndex];
+        var gameSolutions = game.getElementsByTagName('solution')
+        for(var i = 0; i < gameSolutions.length; i++) {
 
-                return ([this.generateDOMItem(0, textArray[Math.floor(Math.random()*textArray.length)], 180),this.generateDOMItem(1, textArray[Math.floor(Math.random()*textArray.length)], 180),this.generateDOMItem(2, textArray[Math.floor(Math.random()*textArray.length)], 180),this.generateDOMItem(3, textArray[Math.floor(Math.random()*textArray.length)], 180)]
-                );
+            var gameSolutionSplitRows = gameSolutions[i].firstChild.nodeValue.split(';');
+            var gameSolutionSplit = new Array(gameSolutionSplitRows.length);
+            for (var j = 0; j < gameSolutionSplitRows.length; j++) {
+                gameSolutionSplit[j] = gameSolutionSplitRows[j].split(',');
             }
-            */
+
+            console.log(gameSolutionSplit);
+
+            for (var k = 0; k < this.state.layout.length; k++) {
+                for (var l = 0; l < this.state.items.length; l++) {
+                    if (this.state.layout[k].i == this.state.items[l].i) {
+                        this.state.layout[k]
+                        this.state.items[l]
+
+                        //if (gameSolutionSplit[this.state.layout[k].x][this.state.layout[k].y] == )
+                    }
+                }
+
+            }
+        }
+
+    }
 
     generateDOM() {
         var items = this.state.items;
@@ -155,13 +99,9 @@ class NoCompactingLayout extends React.PureComponent {
         var randomNumber = Math.floor(Math.random()*textArray.length);
 
         function getOneDOM(item,index){
-            console.log(xmlDoc);
-            var blocks = xmlDoc.getElementsByTagName('rolltheball')[0].getElementsByTagName('blocks')[0].getElementsByTagName('block');
+            var blocks = this.state.xmlDoc.getElementsByTagName('rolltheball')[0].getElementsByTagName('blocks')[0].getElementsByTagName('block');
             return(this.generateDOMItem(item.i,item.imgSource,item.rotation))
         }
-        //console.log("generateDOMReturnValue");
-        //console.log(this.generateDOMItem(items[0].i,textArray[Math.floor(Math.random()*textArray.length)],90));
-        //console.log(getOneDOM(items[0],0,this));
         return(items.map(getOneDOM,this));
 
     }
@@ -172,9 +112,10 @@ class NoCompactingLayout extends React.PureComponent {
     )
     }
     pickGame(gameSelectionIndex){
-        xmlDoc.getElementsByTagName('block');
-        var game = xmlDoc.getElementsByTagName('rolltheball')[0].getElementsByTagName('games')[0].getElementsByTagName('game')[gameSelectionIndex];
-        var blocks = xmlDoc.getElementsByTagName('rolltheball')[0].getElementsByTagName('blocks')[0].getElementsByTagName('block');
+        console.log(this.state.xmlDoc);
+        this.state.xmlDoc.getElementsByTagName('block');
+        var game = this.state.xmlDoc.getElementsByTagName('rolltheball')[0].getElementsByTagName('games')[0].getElementsByTagName('game')[gameSelectionIndex];
+        var blocks = this.state.xmlDoc.getElementsByTagName('rolltheball')[0].getElementsByTagName('blocks')[0].getElementsByTagName('block');
         sizeH = parseInt(game.getElementsByTagName('size')[0].getElementsByTagName('horizontal')[0].firstChild.nodeValue);
         sizeW = parseInt(game.getElementsByTagName('size')[0].getElementsByTagName('vertical')[0].firstChild.nodeValue);
         var gameTask = game.getElementsByTagName('task')[0].firstChild.nodeValue;
@@ -184,14 +125,13 @@ class NoCompactingLayout extends React.PureComponent {
             gameTaskSplit[i] = gameTaskSplitRows[i].split(',');
         }
 
-        console.log(sizeW);
-        console.log(sizeH);
-        console.log(gameTaskSplit);
-
         this.setState({
             width: sizeW*100
         });
-        this.state.items = [];
+        this.setState({
+            items: []
+        });
+
         var counter = 0;
         for (var i = 0;i <gameTaskSplit.length;i++)
         {
@@ -223,15 +163,6 @@ class NoCompactingLayout extends React.PureComponent {
         this.setState({
             layout: this.state.items
         });
-/*
-        var layoutNew = this.state.layout;
-        this.state.items.push(this.generateItem(4,4,"x",false,"T120"))
-        this.setState({
-            layout: layoutNew
-        });
-        console.log(layoutNew);
-        this.forceUpdate();
-*/
     }
 
     generateItem(xI,yI,idI,staticI,typeI,imgSourceI,rotationI) {
@@ -239,7 +170,7 @@ class NoCompactingLayout extends React.PureComponent {
     }
 
     render() {
-        if(xmlDoc) {
+        if(this.state.xmlDoc) {
             return (
                 <div>
                     <ReactGridLayout layout={this.state.layout}
@@ -261,18 +192,13 @@ class NoCompactingLayout extends React.PureComponent {
 
 
                     </ReactGridLayout>
-                    <button onClick={this.resetLayout}>Reset Layout</button>
                     <select id="gameNumber">
                         <option value="0">0</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
                     </select>
+                    <button onClick={() => this.checkWin(document.getElementById("gameNumber").options[document.getElementById("gameNumber").selectedIndex].value)}>CheckWin</button>
                     <button onClick={() => this.pickGame(document.getElementById("gameNumber").options[document.getElementById("gameNumber").selectedIndex].value)}>LoadGame</button>
-                    <button onClick={() =>
-                        this.setState({
-                            layout: layoutStatic2
-                        })
-                    }>Static2</button>
                 </div>
             )
         }
@@ -281,41 +207,6 @@ class NoCompactingLayout extends React.PureComponent {
         );
     }
 }
-
-/*
-{this.generateDOMItem("0","box.png",0)}
-                {this.generateDOMItem("1","start.png",180)}
-                {this.generateDOMItem("2","box.png",0)}
-                {this.generateDOMItem("3","box.png",0)}
-                {this.generateDOMItem("4","box.png",0)}
-                {this.generateDOMItem("5","direct.png",0)}
-                {this.generateDOMItem("6","box.png",0)}
-                {this.generateDOMItem("7","box.png",0)}
-                {this.generateDOMItem("8","box.png",0)}
-                {this.generateDOMItem("9","turn.png",0)}
-                {this.generateDOMItem("10","turn.png",180)}
-                {this.generateDOMItem("11","box.png",0)}
-                {this.generateDOMItem("12","turn.png",0)}
-                {this.generateDOMItem("13","finish.png",270)}
-                {this.generateDOMItem("14","turn.png",270)}
-                {this.generateDOMItem("15","turn.png",270)}
-
-<button onClick={this.pickGame(document.getElementById("gameNumber").options[document.getElementById("gameNumber").selectedIndex].value)}>LoadGame</button>
-<button onClick={this.resetLayout}>Reset Layout</button>
-<div key="a"><span className="text">a</span></div>
-                <div key="b"><span className="text">b</span></div>
-                <div key="c"><span className="text">c</span></div>
-                <div key="d"><span className="text">d</span></div>
-                <div key="e"><span className="text">e</span></div>
-                <div key="f"><span className="text">f</span></div>
-                <div key="g"><span className="text">g</span></div>
-                <div key="h"><span className="text">h</span></div>
-                <div key="j"><span className="text">j</span></div>
-                <div key="k"><span className="text">k</span></div>
-                <div key="l"><span className="text">l</span></div>
-                <div key="m"><span className="text">m</span></div>
-                <div key="n"><span className="text">n</span></div>
-                <div key="p"><span className="text">p</span></div> */
 
 function getFromLS(key) {
     let ls = {};
