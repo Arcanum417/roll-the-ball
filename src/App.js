038f42ec3d98c3b9f9e9ea172ec42b7ca2b27267
Arcanum417 <lubos.len@gmail.com>
2017-12-04 17:28:12 +0100

    HryBezTypov

--- src/App.js
+++ src/App.js
@@ -24,11 +24,11 @@ function myFunction(xml) {
 
 const layoutStatic =
     [
-        {i: 'a', x: 0, y: 0, w: 1, h: 1},
-        {i: 'b', x: 1, y: 0, w: 1, h: 1,static: true},
-        {i: 'c', x: 2, y: 0, w: 1, h: 1},
-        {i: 'd', x: 3, y: 0, w: 1, h: 1,},
-        {i: 'e', x: 0, y: 1, w: 1, h: 1},
+        {i: '0', x: 0, y: 0, w: 1, h: 1},
+        {i: '1', x: 1, y: 0, w: 1, h: 1,static: true},
+        {i: '2', x: 2, y: 0, w: 1, h: 1},
+        {i: '3', x: 3, y: 0, w: 1, h: 1,},
+        {i: '4', x: 0, y: 1, w: 1, h: 1},
         {i: 'f', x: 1, y: 1, w: 1, h: 1},
         {i: 'g', x: 2, y: 1, w: 1, h: 1,},
         {i: 'h', x: 3, y: 1, w: 1, h: 1},
@@ -39,20 +39,66 @@ const layoutStatic =
         {i: 'n', x: 1, y: 3, w: 1, h: 1},
         {i: 'p', x: 3, y: 3, w: 1, h: 1, static: true, type: "D180"}
     ];
+
+const layoutStatic2 =
+    [
+        {i: '0', x: 0, y: 3, w: 1, h: 1},
+        {i: '1', x: 1, y: 0, w: 1, h: 1,static: true},
+        {i: '2', x: 2, y: 0, w: 1, h: 1},
+        {i: '3', x: 3, y: 0, w: 1, h: 1,},
+        {i: '4', x: 0, y: 1, w: 1, h: 1},
+        {i: 'f', x: 1, y: 1, w: 1, h: 1},
+        {i: 'g', x: 2, y: 1, w: 1, h: 1,},
+        {i: 'h', x: 3, y: 1, w: 1, h: 1},
+        {i: 'j', x: 1, y: 2, w: 1, h: 1},
+        {i: 'k', x: 2, y: 2, w: 1, h: 1},
+        {i: 'l', x: 3, y: 2, w: 1, h: 1},
+        {i: 'm', x: 0, y: 3, w: 1, h: 1},
+        {i: 'n', x: 1, y: 3, w: 1, h: 1},
+        {i: 'p', x: 3, y: 3, w: 1, h: 1, static: true, type: "D180"}
+    ];
+
 var sizeW=4;
 var sizeH=4;
 const originalLayout = getFromLS('layout') || layoutStatic;
 class NoCompactingLayout extends React.PureComponent {
 
+    resolveAfter2Seconds(x) {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve(x);
+            }, 100);
+        });
+    }
+
+    async componentDidMount() {
+        var d = await this.resolveAfter2Seconds(10)
+        console.log(xmlDoc);
+        this.pickGame(0);
+    }
+/*
+    componentDidMount(){
+        console.log(this.state.xmlDoc);
+        //this.pickGame(0);
+
+    }
+*/
     constructor(props) {
         super(props);
 
+
+        const layoutStatic3 =
+            [
+                {i: '0', x: 0, y: 0, w: 1, h: 1},
+            ];
         var score = 9;
 
         this.state = {
             //layout: JSON.parse(JSON.stringify(originalLayout)),
-            items: [],
-            layout: []
+            items: layoutStatic3,
+            layout: [],
+            score,
+            layoutStatic3
         };
         this.onLayoutChange = this.onLayoutChange.bind(this);
         this.resetLayout = this.resetLayout.bind(this);
@@ -60,7 +106,7 @@ class NoCompactingLayout extends React.PureComponent {
 
     resetLayout() {
         this.setState({
-            layout: layoutStatic
+            layout: this.state.items
         });
         console.log("nope");
     }
@@ -68,7 +114,7 @@ class NoCompactingLayout extends React.PureComponent {
     onLayoutChange(layout) {
         console.log("onLayoutChange!", layout);
         //saveToLS('layout', layout);
-        this.setState({layout});
+        //this.setState({layout});
         //this.props.onLayoutChange(layout);
 
     }
@@ -83,11 +129,43 @@ class NoCompactingLayout extends React.PureComponent {
         });
     }
 */
+    /*
+            generateDOM() {
+                console.log("generateDOM");
+                var textArray = [
+                    'direct.png',
+                    'turn.png',
+                    'box.png'
+                ];
+                var randomNumber = Math.floor(Math.random()*textArray.length);
+
+                return ([this.generateDOMItem(0, textArray[Math.floor(Math.random()*textArray.length)], 180),this.generateDOMItem(1, textArray[Math.floor(Math.random()*textArray.length)], 180),this.generateDOMItem(2, textArray[Math.floor(Math.random()*textArray.length)], 180),this.generateDOMItem(3, textArray[Math.floor(Math.random()*textArray.length)], 180)]
+                );
+            }
+            */
+
     generateDOM() {
-        console.log("generateDOM");
-        return ([this.generateDOMItem(0, "direct.png", 180),this.generateDOMItem(1, "turn.png", 180),this.generateDOMItem(2, "direct.png", 180),this.generateDOMItem(3, "turn.png", 180)]
-        );
+        var items = this.state.items;
+
+        var textArray = [
+            'direct.png',
+            'turn.png',
+            'box.png'
+        ];
+        var randomNumber = Math.floor(Math.random()*textArray.length);
+
+        function getOneDOM(item,index){
+            var blocks = xmlDoc.getElementsByTagName('rolltheball')[0].getElementsByTagName('blocks')[0].getElementsByTagName('block');
+            console.log(item.type);
+            return(this.generateDOMItem(item.i,textArray[Math.floor(Math.random()*textArray.length)],90))
+        }
+        //console.log("generateDOMReturnValue");
+        //console.log(this.generateDOMItem(items[0].i,textArray[Math.floor(Math.random()*textArray.length)],90));
+        //console.log(getOneDOM(items[0],0,this));
+        return(items.map(getOneDOM,this));
+
     }
+
     generateDOMItem(keyp,imageSource,rotate){
         return (
             <div key={keyp}><img src={"img/" + imageSource } className={"rotate" + rotate + " gridItemImg"} /></div>
@@ -96,7 +174,7 @@ class NoCompactingLayout extends React.PureComponent {
     pickGame(gameSelectionIndex){
         xmlDoc.getElementsByTagName('block');
         var game = xmlDoc.getElementsByTagName('rolltheball')[0].getElementsByTagName('games')[0].getElementsByTagName('game')[gameSelectionIndex];
-        var blocks = xmlDoc.getElementsByTagName('rolltheball')[0].getElementsByTagName('blocks')[0].getElementsByTagName('block');
+
         sizeH = parseInt(game.getElementsByTagName('size')[0].getElementsByTagName('horizontal')[0].firstChild.nodeValue);
         sizeW = parseInt(game.getElementsByTagName('size')[0].getElementsByTagName('vertical')[0].firstChild.nodeValue);
         var gameTask = game.getElementsByTagName('task')[0].firstChild.nodeValue;
@@ -106,7 +184,6 @@ class NoCompactingLayout extends React.PureComponent {
             gameTaskSplit[i] = gameTaskSplitRows[i].split(',');
         }
 
-        console.log(blocks);
         console.log(sizeW);
         console.log(sizeH);
         console.log(gameTaskSplit);
@@ -114,22 +191,25 @@ class NoCompactingLayout extends React.PureComponent {
         this.setState({
             width: sizeW*100
         });
-        this.forceUpdate();
+        this.state.items = [];
         var counter = 0;
         for (var i = 0;i <gameTaskSplit.length;i++)
         {
             for (var j = 0;j <gameTaskSplit[i].length;j++)
             {
-                this.state.items.push(this.generateItem(i,j,counter.toString(),false));//,gameTaskSplit[i][j].toString()
+                this.state.items.push(this.generateItem(j,i,counter.toString(),false,gameTaskSplit[i][j].toString()));//
                 counter++;
             }
         }
-        console.log(this.state.items);
+        //remove type empty, we dont need them
+        for(var i = this.state.items.length - 1; i >= 0; i--) {
+            if(this.state.items[i].type === "E") {
+                this.state.items.splice(i, 1);
+            }
+        }
         this.setState({
             layout: this.state.items
         });
-        console.log(this.generateDOM());
-        this.forceUpdate();
 /*
         var layoutNew = this.state.layout;
         this.state.items.push(this.generateItem(4,4,"x",false,"T120"))
@@ -137,42 +217,54 @@ class NoCompactingLayout extends React.PureComponent {
             layout: layoutNew
         });
         console.log(layoutNew);
+        this.forceUpdate();
 */
     }
 
     generateItem(xI,yI,idI,staticI,typeI) {
-        return {x: xI, y: yI, w: 1, h: 1, i: idI, static: staticI, type: typeI};
+        return {i: idI, x: xI, y: yI, w: 1, h: 1, static: staticI, type: typeI};
     }
 
     render() {
+        if(xmlDoc) {
+            return (
+                <div>
+                    <ReactGridLayout layout={this.state.layout}
+                                     onLayoutChange={this.onLayoutChange}
+                                     cols={sizeW}
+                                     rowHeight={100}
+                                     maxRows={sizeH}
+                                     isResizable={false}
+                                     autoSize={true}
+                                     compactType={null}
+                                     preventCollision={true}
+                                     width={sizeW*100}
+                                     margin={[0,0]}
+                                     style={{width : sizeW*100}}
+                                     {...this.props}
+                    >
+                        {this.generateDOM()}
+
+
+
+                    </ReactGridLayout>
+                    <button onClick={this.resetLayout}>Reset Layout</button>
+                    <select id="gameNumber">
+                        <option value="0">0</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                    </select>
+                    <button onClick={() => this.pickGame(document.getElementById("gameNumber").options[document.getElementById("gameNumber").selectedIndex].value)}>LoadGame</button>
+                    <button onClick={() =>
+                        this.setState({
+                            layout: layoutStatic2
+                        })
+                    }>Static2</button>
+                </div>
+            )
+        }
         return (
-            <div>
-            <ReactGridLayout layout={this.state.layout}
-                             onLayoutChange={this.onLayoutChange}
-                             cols={sizeW}
-                             rowHeight={100}
-                             maxRows={sizeH}
-                             isResizable={false}
-                             autoSize={true}
-                             compactType={null}
-                             preventCollision={true}
-                             width={sizeW*100}
-                             margin={[0,0]}
-                             style={{width : sizeW*100}}
-                             {...this.props}
-            >
-                {this.generateDOM()}
-
-
-            </ReactGridLayout>
-            <button onClick={this.resetLayout}>Reset Layout</button>
-            <select id="gameNumber">
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-            </select>
-            <button onClick={() => this.pickGame(document.getElementById("gameNumber").options[document.getElementById("gameNumber").selectedIndex].value)}>LoadGame</button>
-            </div>
+            "Loading..."
         );
     }
 }
